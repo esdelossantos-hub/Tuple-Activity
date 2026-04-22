46fa212ddcd6a6617e809445dff509893d3fb5d4
esdelossantos-hub <esdelossantos@paterostechnologicalcollege.edu.ph>
2026-04-22 14:53:52 +0800

    Tuple Activity_gr5_DelosSantos.py

---
+++ Tuple Activity_gr5_DelosSantos.py
@@ -0,0 +1,24 @@
+colors = ("red", "green", "blue")
+print(colors[0])   
+print(colors[-1])  
+
+numbers = (10, 20, 30, 40, 50)
+print(numbers[1:4])   # (20, 30, 40)
+
+nums = (1, 2, 2, 3, 2)
+
+print(nums.count(2))  
+print(nums.index(3))   
+
+point = (5, 10)
+
+x, y = point
+
+print("x =", x)
+print("y =", y)
+
+t1 = (1, 2)
+t2 = (3, 4)
+
+combined = t1 + t2
+print(combined * 2)
